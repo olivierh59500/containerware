6e5cc82e6cb3ec98f64626b9bb1804cd818ad5cd
Mo McRoberts <mo@nevali.net>
2013-04-01 01:26:00 +0100

    Add placeholder for libship

---
+++ libship/p_ship.h
@@ -0,0 +1,10 @@
+#ifndef P_SHIP_H_
+# define P_SHIP_H_                     1
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+# include "ship.h"
+
+#endif /*!P_SHIP_H_*/
---
+++ libship/ship.h
@@ -0,0 +1,6 @@
+#ifndef SHIP_H_
+# define SHIP_H_                       1
+
+# include "containerware.h"
+
+#endif /*!SHIP_H_*/
